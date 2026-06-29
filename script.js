29ce2f5aa0dfea423448227efa6f27686d77792e
mezaluna9-code <mezaluna9@gmail.com>
2026-06-29 19:13:25 +0700

    Add loader animation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+window.addEventListener("load", function () {
 
+    const loader = document.getElementById("loader");
+
+    if (loader) {
+
+        loader.style.transition = "opacity 0.8s ease";
+        loader.style.opacity = "0";
+
+        setTimeout(function () {
+            loader.style.display = "none";
+        }, 800);
+
+    }
+
+});
