000b337eeef483514ef1638a26880b13f3a7601b
Nindi <mezaluna9@gmail.com>
2026-07-02 19:52:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,36 @@ window.addEventListener("load", function () {
     }
 
 });
+const worlds = document.querySelectorAll(".world-card");
+
+const popup = document.getElementById("world-popup");
+
+const closeBtn = document.querySelector(".close-popup");
+
+worlds.forEach(world => {
+
+    world.style.cursor = "pointer";
+
+    world.addEventListener("click", () => {
+
+        popup.style.display = "flex";
+
+    });
+
+});
+
+closeBtn.addEventListener("click", () => {
+
+    popup.style.display = "none";
+
+});
+
+window.addEventListener("click",(e)=>{
+
+    if(e.target===popup){
+
+        popup.style.display="none";
+
+    }
+
+});
